aefcfbb3eddfe0c6db547bb0e855ee347c10c447
JeffTunner <abhinavkaushik05@gmail.com>
2026-07-01 17:30:47 +0530

    feat: patient service

---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+}
