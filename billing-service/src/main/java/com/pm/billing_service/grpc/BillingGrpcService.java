c7ff6dd1e284586b2d9e540f3c48a4f0550ad428
JeffTunner <abhinavkaushik05@gmail.com>
2026-07-05 21:09:02 +0530

    feat: patient-service successfully a grpc client

--- billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -16,7 +16,7 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
     @Override
     public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
 
-        log.info("createBillingAccount request received {} ", billingRequest);
+        log.info("createBillingAccount request received {} ", billingRequest.toString());
 
         //Business logic
 
