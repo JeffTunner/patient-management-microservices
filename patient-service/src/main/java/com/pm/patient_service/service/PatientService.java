e40aa2530c7a57a9fb3b933fe43bc21421de7582
JeffTunner <abhinavkaushik05@gmail.com>
2026-07-16 22:39:35 +0530

    feat: incorporated kafka event into patient service

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
 import com.pm.patient_service.grpc.BillingServiceGrpcClient;
+import com.pm.patient_service.kafka.KafkaProducer;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -24,6 +25,9 @@ public class PatientService {
     @Autowired
     BillingServiceGrpcClient grpcClient;
 
+    @Autowired
+    KafkaProducer kafkaProducer;
+
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
         return patients.stream().map(PatientMapper::toDTO).toList();
@@ -37,6 +41,8 @@ public class PatientService {
         Patient saved = patientRepository.save(patient);
 
         grpcClient.createBillingAccount(saved.getId().toString(), saved.getName(), saved.getEmail());
+
+        kafkaProducer.sendEvent(saved);
         return PatientMapper.toDTO(saved);
     }
 
